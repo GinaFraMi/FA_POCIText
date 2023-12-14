ade4af4393cd2ccb68a66bff02d1211d2fa67ae8
GeorginaFraire <georgina.fraire@wizeline.com>
2023-12-14 11:02:17 -0600

    support to others fonts

--- src/PocItext/Services/CreatePDF.cs
+++ src/PocItext/Services/CreatePDF.cs
@@ -1,23 +1,37 @@
-using System;
-using System.IO;
 using iText.Html2pdf;
-using iText.Kernel.Pdf;
 using HandlebarsDotNet;
+using iText.Html2pdf.Resolver.Font;
+using iText.Layout.Font;
+using iText.IO.Font;
 
 namespace PocItext.Services
 {
   public class CreatePDF
   {
 
-    public byte[] GeneratePDF<T>(string template, T properties)
+    public byte[] GeneratePDF<T>(string template, T obj)
     {
       using (MemoryStream output = new MemoryStream())
       {
         string htmlContent = File.ReadAllText(template);
         var handler = Handlebars.Compile(htmlContent);
-        string result = handler(properties);
-        HtmlConverter.ConvertToPdf(result, output);
-       return output.ToArray();
+        string result = handler(obj);
+
+        string directory = "Fonts";
+        var fontPaths = Directory.GetFiles(directory, "*.ttf");
+        var fonts = fontPaths.Select(path => FontProgramFactory.CreateFont(path, PdfEncodings.IDENTITY_H, true)).ToList();
+
+
+        ConverterProperties properties = new ConverterProperties();
+        FontProvider fontProvider = new DefaultFontProvider();
+        foreach (var fontPath in fontPaths)
+        {
+          fontProvider.AddFont(fontPath);
+        }
+        properties.SetFontProvider(fontProvider);
+        HtmlConverter.ConvertToPdf(result, output, properties);
+        
+        return output.ToArray();
       }
     }
   }
