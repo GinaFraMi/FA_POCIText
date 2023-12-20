8b26783bdc50a3bd42267e0975019d082fc51998
GeorginaFraire <georgina.fraire@wizeline.com>
2023-12-20 10:05:28 -0600

    Change Generate PDF service

--- src/PocItext/Services/CreatePDF.cs
+++ src/PocItext/Services/CreatePDF.cs
@@ -1,38 +1,52 @@
 using iText.Html2pdf;
 using HandlebarsDotNet;
-using iText.Html2pdf.Resolver.Font;
 using iText.Layout.Font;
+using iText.Html2pdf.Resolver.Font;
 using iText.IO.Font;
 
 namespace PocItext.Services
 {
   public class CreatePDF
-  {
+   {
 
-    public byte[] GeneratePDF<T>(string template, T obj)
+    private FontProvider CreateFonts()
+    {
+
+      string directory = "Fonts";
+      var executionPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+      var pathString = Path.Combine(executionPath!, directory);
+      var fontPaths = Directory.GetFiles(pathString, "*.ttf");
+      var fonts = fontPaths.Select(path => FontProgramFactory.CreateFont(path, PdfEncodings.IDENTITY_H, true)).ToList();
+      FontProvider fontProvider = new DefaultFontProvider();
+      foreach (var fontPath in fontPaths)
+      {
+        fontProvider.AddFont(fontPath);
+      }
+      return fontProvider;
+    }
+
+    private byte[] PDF (string content)
     {
       using (MemoryStream output = new MemoryStream())
       {
-        string htmlContent = File.ReadAllText(template);
+      ConverterProperties properties = new ConverterProperties();
+      properties.SetFontProvider(CreateFonts());
+      HtmlConverter.ConvertToPdf(content, output, properties);
+      return output.ToArray();
+      }
+    }
+
+    public byte[] GeneratePDF<T>(string htmlContent, T obj)
+    {
+
         var handler = Handlebars.Compile(htmlContent);
         string result = handler(obj);
+        return PDF(result);
+    }
 
-        string directory = "Fonts";
-        var fontPaths = Directory.GetFiles(directory, "*.ttf");
-        var fonts = fontPaths.Select(path => FontProgramFactory.CreateFont(path, PdfEncodings.IDENTITY_H, true)).ToList();
-
-
-        ConverterProperties properties = new ConverterProperties();
-        FontProvider fontProvider = new DefaultFontProvider();
-        foreach (var fontPath in fontPaths)
-        {
-          fontProvider.AddFont(fontPath);
-        }
-        properties.SetFontProvider(fontProvider);
-        HtmlConverter.ConvertToPdf(result, output, properties);
-        
-        return output.ToArray();
-      }
+    public byte[] GeneratePDF(string htmlContent)
+    {
+      return PDF(htmlContent);
     }
   }
 }
